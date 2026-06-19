477b878e11a6f2420be24c80d23a137c61029f03
Joey <j.casadonte04@gmail.com>
2026-06-19 15:41:18 -0400

    made readiness public

--- app/dashboard/readiness-screen/page.tsx
+++ app/dashboard/readiness-screen/page.tsx
@@ -1,10 +1,5 @@
-import { AdminGuard } from "@/app/dashboard/AdminGuard";
 import { ReadinessScreenContent } from "./ReadinessScreenContent";
 
 export default function ReadinessScreenPage() {
-  return (
-    <AdminGuard>
-      <ReadinessScreenContent />
-    </AdminGuard>
-  );
+  return <ReadinessScreenContent />;
 }
